84eeda926894736551bceaf9da892c8bbde7c3c0
Shalmon Anandas <nashalanandas@gmail.com>
2023-10-04 20:10:38 +0530

    ui changes

--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -37,7 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           (searchController.text.length > 2)
               ? SearchResultScreen(searchTerm: searchController.text)
-              : const UnderConstructionScreen(title: "yee"),
+              : const SizedBox.shrink(),
         ],
       ),
     );
